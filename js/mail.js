55c7bed6e777f023bfd3572812113304f68a1f53
bennyworkz <bennyabhishikth.works@gmail.com>
2024-09-26 00:00:45 +0530

    contact form issue solved

--- js/mail.js
+++ js/mail.js
@@ -16,7 +16,7 @@ document.getElementById('btn').addEventListener('click', function (e) {
         travelCount,
         message
     };
-
+    // https://wandereasebackend.onrender.com
     fetch('https://wandereasebackend.onrender.com/api/send-email', {
         method: 'POST',
         headers: {
@@ -58,39 +58,6 @@ document.getElementById('btn1').addEventListener('click', function (e) {
     };
 
     fetch('https://wandereasebackend.onrender.com/api/send-email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert('Email sent successfully!');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Failed to send email.');
-    });
-});
-
-
-document.getElementById('btn2').addEventListener('click', function (e) {
-    e.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const message = document.getElementById('message').value;
-
-    const data = {
-        name,
-        email,
-        phone,
-        message
-    };
-
-    fetch('https://wandereasebackend.onrender.com/api/send-email1', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
