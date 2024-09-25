867ed569eef07fda97f63754b9f40d9a947447d5
bennyworkz <bennyabhishikth.works@gmail.com>
2024-09-25 22:31:35 +0530

    script for contact details mail

--- js/mail.js
+++ js/mail.js
@@ -73,3 +73,36 @@ document.getElementById('btn1').addEventListener('click', function (e) {
         alert('Failed to send email.');
     });
 });
+
+
+document.getElementById('btn2').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    const data = {
+        name,
+        email,
+        phone,
+        message
+    };
+
+    fetch('https://wandereasebackend.onrender.com/api/send-email1', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Email sent successfully!');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Failed to send email.');
+    });
+});
